4d3e578c6ce592be537f33c1b91cb8415c37bfbb
Isaaruwu <ismailaarabb@gmail.com>
2023-06-30 11:13:02 -0400

    ♻ refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn create_directory(project_path: &path::PathBuf) {
     fs::create_dir(&project_path).unwrap();
 }
 
-fn create_project(_command: &str, _language: &str, _project_type: Option<&str>, project_name: &str, path: Option<&str>) {
+fn create_project(language: &str, project_type: Option<&str>, project_name: &str, path: Option<&str>) {
     let current_dir = env::current_dir().unwrap();
     let mut project_path = path::Path::new(&current_dir).join(project_name);
 
@@ -28,10 +28,13 @@ fn create_project(_command: &str, _language: &str, _project_type: Option<&str>, 
             return;
         }
     }
-    
-    create_directory(&project_path);
-    create_python_project(&project_path);
 
+    create_directory(&project_path);
+
+    match language {
+        "py" => create_python_project(&project_path, &project_type),
+        _ => log::warn!("Invalid language, please use 'py'"),
+    }
 }
 fn main() {
     SimpleLogger::new().init().unwrap();
@@ -40,39 +43,30 @@ fn main() {
     let languages = ["py"];
 
     let matches = App::new("My CLI App")
-        .arg(Arg::with_name("command")
-            .help("The command to execute")
-            .required(true)
-            .index(1))
         .arg(Arg::with_name("language")
             .help("The programming language")
             .possible_values(&languages)
             .required(true)
-            .index(2))
+            .index(1))
         .arg(Arg::with_name("project_name")
             .help("The name of the project")
             .required(true)
-            .index(3))
+            .index(2))
         .arg(Arg::with_name("project_type")
             .help("The type of project")
             .possible_values(&project_types)
             .required(false)
-            .index(4))
+            .index(3))
         .arg(Arg::with_name("path")
             .help("The path to use (optional)")
-            .index(5))
+            .index(4))
         .get_matches();
 
-    let command = matches.value_of("command").unwrap();
     let language = matches.value_of("language").unwrap();
     let project_type = matches.value_of("project_type");
     let project_name = matches.value_of("project_name").unwrap();
     let path = matches.value_of("path");
 
-    if command == "create" {
-        create_project(command, language, project_type, project_name, path);
-    } else {
-        log::warn!("Invalid command, please use 'create'");
-        println!("Command not found");
-    }
+    create_project(language, project_type, project_name, path);
+    
 }
